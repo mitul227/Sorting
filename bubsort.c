f7574a91a6e96146a1b0d8f3ba35b0d100f7709c
mitul227 <mituljoshi227@gmail.com>
2015-10-14 18:53:24 +0530

    Sorting

--- bubsort.c
+++ bubsort.c
@@ -1,18 +1,18 @@
 void main()
 {
-    int a[1000],i,j,t,n;
+    int a[1000],i,j,t,n,k;
     scanf("%d",&n);
     for(i=0;i<n;i++)
     scanf("%d",&a[i]);
-    for(i=n-1;i>0;i--)
+    for(i=0;i<n-1;i++)
     {
-        for(j=i-1;j>=0;j--)
+        for(j=0;j<n-i-1;j++)
         {
-            if(a[i]<a[j])
+            if(a[j]>a[j+1])
             {
-                t=a[i];
-                a[i]=a[j];
-                a[j]=t;
+                t=a[j];
+                a[j]=a[j+1];
+                a[j+1]=t;
             }
         }
     }
--- selsort.c
+++ selsort.c
@@ -1,20 +1,22 @@
 void main()
 {
-    int a[1000],i,j,t,n;
+    int a[1000],i,j,t,n,k,min;
     scanf("%d",&n);
     for(i=0;i<n;i++)
     scanf("%d",&a[i]);
     for(i=0;i<n-1;i++)
     {
+        min=i;
         for(j=i+1;j<n;j++)
         {
-            if(a[i]>a[j])
+            if(a[j]<a[min])
             {
-                t=a[i];
-                a[i]=a[j];
-                a[j]=t;
+                min=j;
             }
         }
+        t=a[min];
+        a[min]=a[i];
+        a[i]=t;
     }
     for(i=0;i<n;i++)
     printf("%d ",a[i]);
